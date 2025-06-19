bc6afc7979499bab90f6cf48b106e13534f58aae
Luiz Ed <luis.epenha@localiza.com>
2025-06-18 21:06:52 -0300

    wip: starting to evolve

--- poc.js
+++ poc.js
@@ -1,13 +1,13 @@
 const DIRTY_ENTRY_STRING = `
-Administração de CRLV Digital
+SPOC - Atendimento 24HSPOC - Aviso SinistroSPOC - Consulta FornecedorSPOC - Consulta Movimentações do Veículo (RAC)SPOC - Consulta OrçamentosSPOC - Digitalização - SSSPOC - Fila de Aprovação de Orçamentos
 `;
 
 const { readData, writeData } = require('./fsUtils.js');
 
 const sanitizeEntryString = (inputString) => inputString
-  .split(/SPOC - /) // Divide a string onde encontra "SPOC - "
-  .filter(desc => desc.trim() !== "") // Remove entradas vazias
-  .map(desc => desc.trim()); // Remove espaços extras
+  .split(/SPOC - /) // Splits "SPOC - "
+  .filter(desc => desc.trim() !== "") // Empty entries
+  .map(desc => desc.trim()); // to be shure
 
 async function processAndMatch(imputString) {
   const SOURCE_OF_TRUTH = await readData();  
@@ -16,9 +16,9 @@ async function processAndMatch(imputString) {
   const screensMatched = [];
 
   screensDemanded
-  .forEach((screenNameDemanded) => 
+  .forEach((screenDemandedName) => 
     screensMatched.push(
-      ...SOURCE_OF_TRUTH.filter((screenData) => screenData.name === screenNameDemanded)
+      ...SOURCE_OF_TRUTH.filter((screenData) => screenData.name === screenDemandedName)
         .map((matched) => matched.spocValues.nonCritical))
   )
 return screensMatched.flat();
@@ -30,3 +30,28 @@ await writeData(runned)
 return;
 }
 main();
+
+
+
+/* TODO #1
+
+Ao enviar o verbo POST, verificar, das telas do payload, se todas já se encontram no banco
+
+O retorno deve ser o código das telas, mas, também a informação que tela X não foi encontrada
+
+EX:
+  payload:
+SPOC - Tela de Fazer IssoSPOC - Tela faz Aquilo
+
+output:
+
+5413
+3546
+
+"SPOC - Tela faz Aquilo" não encontrada
+
+
+TODO #2
+
+Processar os GF, mas cortando todos os críticos, com "sup" no nome.
+*/
