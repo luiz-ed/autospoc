580dcc27d3b0061c2c18b0578be3ede803639d88
Luiz Ed <luis.epenha@localiza.com>
2025-09-09 01:47:50 -0300

    :hameer: fixed swagger src #3

--- teste.js
+++ teste.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Methods", '*');
+  res.set("Access-Control-Allow-Methods", '*');
   
   next();
 });
